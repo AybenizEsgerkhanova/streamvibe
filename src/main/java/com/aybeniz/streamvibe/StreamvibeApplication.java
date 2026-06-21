cf6dfc42af967cdbb24fb34c5afa0f888e51fcb1
Aybeniz <aybenizesgerkhanova@gmail.com>
2026-06-21 20:45:10 +0400

    init commit

---
+++ src/main/java/com/aybeniz/streamvibe/StreamvibeApplication.java
@@ -0,0 +1,13 @@
+package com.aybeniz.streamvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamvibeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamvibeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aybeniz/streamvibe/StreamvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aybeniz.streamvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamvibeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
